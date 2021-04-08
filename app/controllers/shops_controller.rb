d3c3240b3fc7691c20251e0c2ba37870924c45bc
Marko Glibota <markoglibota@gmail.com>
2021-04-08 12:45:08 +0200

    Add shop new and create (controller actions + views)

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -6,4 +6,23 @@ class ShopsController < ApplicationController
   def show
     @shop = Shop.find(params[:id])
   end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to shop_path(@shop)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :address)
+  end
 end
