27e69a864381d0db1441c09722980a70ef973fdb
Marko Glibota <markoglibota@gmail.com>
2021-04-08 12:59:32 +0200

    Add shop edit and update (controller actions + views)

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,10 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show, :edit, :update]
   def index
     @shops = Shop.all
   end
 
   def show
-    @shop = Shop.find(params[:id])
   end
 
   def new
@@ -20,7 +20,22 @@ class ShopsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @shop.update(shop_params)
+      redirect_to shop_path(@shop)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
 
   def shop_params
     params.require(:shop).permit(:name, :address)
