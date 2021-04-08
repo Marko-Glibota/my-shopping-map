5d11014263da2bf57a5396975c18672442a7ea62
Marko Glibota <markoglibota@gmail.com>
2021-04-08 13:04:02 +0200

    Add validations to shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,4 @@
 class Shop < ApplicationRecord
+  validates :name, presence: true, allow_nil: false, allow_blank: false
+  validates :address, presence: true, allow_nil: false, allow_blank: false
 end
