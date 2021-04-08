9927b1b46d794ea6685b886730d98d5664600ce1
Marko Glibota <markoglibota@gmail.com>
2021-04-08 16:32:16 +0200

    Add validations to user model for username and address fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, uniqueness: true, allow_nil: false, allow_blank: false
+  validates :address, presence: true, allow_nil: false, allow_blank: false
 end
